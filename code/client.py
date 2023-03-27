dd9f620fc5bf496cc1ac01c67c8ddb7d48be46b3
antoinepolster <20090@ecam.be>
2023-03-27 09:40:47 +0200

    envoyer requete au serveur

--- code/client.py
+++ code/client.py
@@ -1,15 +1,12 @@
 import socket
-import json 
+import json
 
 s = socket.socket()
 
-serverAddress = ('0.0.0.0', 3000)
+serverAddress = ('localhost', 3000)
 s.connect(serverAddress)
 
 with open('joueur.json') as json_data:
-    identitejoueur = json.load(json_data)
-    request = identitejoueur.encode()
-    print(identitejoueur)
+    request = json_data.read().encode()
+    s.send(request)
 
-
-s.send(request)
