88d8cb5f023b69d365eff3b349a84e57765fa95d
antoinepolster <20090@ecam.be>
2023-03-27 12:01:30 +0200

    ping pong succed

--- code/client.py
+++ code/client.py
@@ -3,10 +3,42 @@ import json
 
 s = socket.socket()
 
-serverAddress = ('localhost', 3000)
+serverAddress = ('localhost', 3000) #adresse du serveur 
 s.connect(serverAddress)
 
-with open('joueur.json') as json_data:
-    request = json_data.read().encode()
-    s.send(request)
+port = 8885
 
+data = {
+    "request": "subscribe",
+    "port": port,
+    "name": "antoinepolster",
+    "matricules": ["20090", "20090"]
+ }
+
+
+request = json.dumps(data).encode()
+s.send(request)
+
+response = s.recv(2048).decode()
+s.close()
+
+serverAddress2 = ('0.0.0.0', port) #mon adresse 
+
+with socket.socket() as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(serverAddress2)
+    s.listen()
+    s.settimeout(1) # Pour que l'accept() ne bloque que 1 seconde
+    while True : 
+        try:
+          client, serverAddres = s.accept()
+          with client:
+             message = json.loads(client.recv(2048).decode())
+             if message['request'] == 'ping':
+                pong = json.dumps({'response': 'pong'}).encode()
+                client.send(pong)
+                print('ok')
+             else :
+                pass
+        except socket.timeout:
+           pass
