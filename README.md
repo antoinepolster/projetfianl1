4917c287ce706c790f8d00a1c84e72ff9d40fd6c
antoinepolster <20090@ecam.be>
2023-03-27 09:01:36 +0200

    avancees notees

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Polster Antoine 20090
 __json__ : pour manipuler des fichierq json   
 __socket__ : pour pouvoir discuter avec le serveur  
 __opengl__ : parler de ça
+
+## avancées 
+### lancer le serveur 
+
+## lancer serveur 
+### pwd
+### ls
+### cd projetfinal1/code
+### ls
+### cd PI2CChampionshipRunner-main
+### ls
+
--- code/client.py
+++ code/client.py
@@ -3,11 +3,16 @@ import json
 
 s = socket.socket()
 
-serverAddress = ('0.0.0.0', 8888)
+serverAddress = ('0.0.0.0', 3000)
 s.connect(serverAddress)
 
 with open('test.json') as json_data:
     identitejoueur = json.load(json_data)
     request = 'identitejoueur'.encode()
 
+s.send(request)
+
+response = s.recv(2048).decode()
+
+print(response)
 print(request)
